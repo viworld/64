1642b8b802f02e9af6e197ce24725abb2a9e3bec
BotMakerBD <109944533+wajirr4@users.noreply.github.com>
2022-08-30 04:22:16 +0600

    Update help.py

--- mills/plugins/help.py
+++ mills/plugins/help.py
@@ -7,7 +7,7 @@
 - <code>/help<code>: Get all available commands with detailed information.
 ──────────────────────
 
-©<a href="https://t.me/roldexverse">RoldexVerse</a>
+©<a href="https://t.me/BotMakerBD">BotMakerBD</a>
 """
 import inspect
 import io
