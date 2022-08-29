d4a1fdeec49c1dd3fc894c3e674338667ff4120c
BotMakerBD <109944533+wajirr4@users.noreply.github.com>
2022-08-30 04:31:03 +0600

    Update gen.py

--- mills/plugins/checkers/gen.py
+++ mills/plugins/checkers/gen.py
@@ -5,7 +5,7 @@
 <code>/gen</code> bin || <reply_to_msg> - Generate cards sempai.
 ──────────────────────
 
-©<a href="https://t.me/roldexverse">RoldexVerse</a>
+©<a href="https://t.me/BotMakerBD">BotMakerBD</a>
 """
 
 
