b1116131811e30a1c9234d3e9e5d09972697fae2
BotMakerBD <109944533+wajirr4@users.noreply.github.com>
2022-08-30 04:02:37 +0600

    Update __init__.py

--- mills/__init__.py
+++ mills/__init__.py
@@ -32,7 +32,7 @@ from telethon.sessions import StringSession
 
 start_deploy = """
             ----------------------------------------------------------------------
-                Deploying Millie! Visit @RoldexVerse for updates!!
+                Deploying Millie!
             ----------------------------------------------------------------------
 """
 
